3965499058ad65f0153aa658288ee35a336f4c89
Tkr8kdigital180 <romeo.tagne@ksyered-education.de>
2026-02-08 13:14:40 +0100

    frontend config done

---
+++ frontend/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$'
+      }
+    ]
+  },
+
+  testMatch: ['**/*.spec.ts'],
+  moduleFileExtensions: ['ts', 'html', 'js'],
+  testEnvironment: 'jsdom'
+};
