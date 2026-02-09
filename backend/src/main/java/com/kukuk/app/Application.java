13163585a33c7a2c0b3d269ee8ff98fc280424e4
Tkr8kdigital180 <romeo.tagne@ksyered-education.de>
2026-02-09 16:49:45 +0100

    Springboot app refactored

--- backend/src/main/java/com/kukuk/app/Application.java
+++ backend/src/main/java/com/kukuk/app/Application.java
@@ -1,7 +1,20 @@
 package com.kukuk.app;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Application {
+
     public static void main(String[] args) {
-        System.out.println("Hello Kukuk DevOps!");
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Kukuk DevOps!";
     }
 }
